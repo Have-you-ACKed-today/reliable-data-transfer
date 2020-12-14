719ae58c8b1f3f7b8f1b73586484aa507ecbc559
Tloops <11813207@mail.sustech.edu.cn>
2020-12-14 15:01:48 +0800

    add some comments

--- rdt_message.py
+++ rdt_message.py
@@ -7,9 +7,12 @@ HEADER_LENGTH = 14
 
 
 def unpack(bytes_message: bytes):
-
+    """
+    unpack receiving message to RdtMessage object
+    :param bytes_message: bytes, origin message
+    :return: RdtMessage
+    """
     flags, seq, seq_ack, length, checksum = struct.unpack(rdt_format, bytes_message[0:HEADER_LENGTH])
-
     print(flags, seq, seq_ack, length, checksum)
     assert flags < 6
 
@@ -28,9 +31,8 @@ class RdtMessage(object):
 
     def __init__(self, flags, seq, seq_ack, length=-1, checksum=-1, payload=""):
         """
-        :param syn: SYN, 0 or 1, 1 bit
-        :param fin: SIN, 0 or 1, 1 bit
-        :param ack: ACK, 0 or 1, 1 bit
+        constructor of RdtMessage
+        :param flags: 5 bits reserved, 1 bit syn, 1 bit fin, 1 bit ack, 1 byte
         :param seq: sequence number, 4 bytes
         :param seq_ack: ack number, 4 bytes
         :param checksum: checksum, 1 byte
@@ -46,6 +48,13 @@ class RdtMessage(object):
             self.checksum = checksum
         self.checksum = calc_checksum(struct.pack(rdt_format, flags, seq, seq_ack, self.length, 0) + b[HEADER_LENGTH:])
 
+    def to_byte(self):
+        """
+        turn the message to bytes
+        :return: bytes
+        """
+        return struct.pack(rdt_format, self.flags, self.seq, self.seq_ack, self.length, self.checksum) + b[HEADER_LENGTH:]
+
     def is_syn_set(self):
         return self.flags & 0b100 != 0
 
@@ -56,6 +65,7 @@ class RdtMessage(object):
         return self.flags & 0b001 != 0
 
 
+# this is just for testing
 if __name__ == "__main__":
     b = b'\x04\x00\x00\x00\x10\x00\x00\x00\x11\x00\x00\x00\x05\x62\x48\x45\x4c\x4c\x4f'
     packet = unpack(b)
