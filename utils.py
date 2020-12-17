1323954372497fa189212dcf52bc791a0bcc6e76
chenchen <75118581+LiYichen99@users.noreply.github.com>
2020-12-17 15:48:31 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,21 @@
 
-
 def calc_checksum(payload):
     sum = 0
     for byte in payload:
         sum += byte
     sum = -(sum % 256)
     return sum & 0xFF
+
+
+def make_data_parts(data, part_size):
+    parts = []
+    total = len(data)
+    curr = 0
+
+    while curr < total:
+        part = data[curr:min(total, curr + part_size)]
+        parts.append(part)
+        curr += part_size
+
+    return parts
+
